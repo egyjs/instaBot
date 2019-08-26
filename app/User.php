17fedfba46ae66fc76f261ecb0aeaf0b0273e822
el3zahaby <el3zahaby@gmail.com>
2019-08-27 01:48:36 +0300

    username test4

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends \TCG\Voyager\Models\User
      * @var array
      */
     protected $fillable = [
-        'username', 'email', 'password',
+        'name', 'email', 'password',
     ];
 
     /**
