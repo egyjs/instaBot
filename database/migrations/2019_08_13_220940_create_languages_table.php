c66492443ac6ae674ed09bf15369e620848d0b65
el3zahaby <el3zahaby@gmail.com>
2019-08-27 01:12:46 +0300

    lang

---
+++ database/migrations/2019_08_13_220940_create_languages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLanguagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('language', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('phrase');
+            $table->longText('en')->nullable();
+            $table->longText('ar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('language');
+    }
+}
